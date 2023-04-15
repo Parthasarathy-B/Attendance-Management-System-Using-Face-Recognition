6776795e6b48a76139661d8b4ac78885d778d259
Parthasarathy <81454132+Parthasarathy-B@users.noreply.github.com>
2023-04-15 21:57:04 +0530

    Update recognition.py

--- recognition.py
+++ recognition.py
@@ -64,7 +64,7 @@ def recognize():
 
     def takeAttendance():
         global rno
-        cap=cv2.VideoCapture(2)
+        cap=cv2.VideoCapture(0)
         while True:
             success,img=cap.read()
             imgs=cv2.resize(img,(0,0),None,0.25,0.25)
